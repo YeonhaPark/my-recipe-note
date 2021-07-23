3a6f960c9e72f7569a2e420eef6e15c19799bb6b
박연하 <bag-yeonha@bag-yeonhaui-MacBookAir.local>
2021-07-23 12:18:28 +0900

    feat: make basic app routing codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import { Login, Main } from './components/pages';
+import 'normalize.css';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/main">
+            <Main />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
