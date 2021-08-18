c67710f7e0df20982e1787dd34c345632af1644a
Park Yeonha <yonaprisca@gmail.com>
2021-08-18 09:27:20 +0900

    feat: add chip testing

---
+++ frontend/src/components/atoms/chip.test.tsx
@@ -0,0 +1,27 @@
+import { shallow, mount } from 'enzyme';
+import { Chip, ChipColor } from '../atoms';
+import renderer from 'react-test-renderer';
+
+const chipProps = {
+  label: 'korean',
+  color: 'info' as ChipColor,
+};
+
+const renderChip = () => mount(<Chip {...chipProps} />);
+
+describe('Chip', () => {
+  it('renders Chip', () => {
+    renderChip();
+  });
+
+  it('renders label passed from parent', () => {
+    const chip = renderChip();
+    chip.setProps({ label: 'chinese' });
+    expect(chip.text()).toEqual('chinese');
+  });
+
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Chip {...chipProps} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
