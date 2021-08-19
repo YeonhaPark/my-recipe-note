e1094684d0834cb34ad512950a06c8848a4239dd
YeonhaPark <yonaprisca@gmail.com>
2021-08-19 12:30:04 +0900

    fix: fix drawer component test

---
+++ frontend/src/components/organisms/drawer.test.tsx
@@ -0,0 +1,52 @@
+import { mount } from 'enzyme';
+import { Drawer } from '../organisms';
+import renderer from 'react-test-renderer';
+import { ChipColor } from '../atoms';
+
+const DrawerProps = {
+  onCreateNew: () => {},
+  recipeList: [
+    {
+      id: '1',
+      title: 'taco',
+      ingredients: [],
+      contents: 'asdf',
+      tags: [],
+      createdAt: new Date(),
+      modifiedAt: new Date(),
+    },
+  ],
+  drawerOpen: true,
+  onRecipeClick: () => {},
+  searchWords: 'wer',
+  onSearch: () => {},
+  onSearchClick: () => {},
+};
+
+const renderDrawer = () => mount(<Drawer {...DrawerProps} />);
+
+describe('Drawer', () => {
+  it('renders Drawer', () => {
+    renderDrawer();
+  });
+
+  it('renders exact prop provided', () => {
+    const drawer = renderDrawer();
+
+    expect(drawer.prop('drawer')).toEqual([
+      {
+        label: 'korean',
+        color: 'info' as ChipColor,
+      },
+      {
+        label: 'japanese',
+        color: 'error' as ChipColor,
+      },
+    ]);
+  });
+
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Drawer {...DrawerProps} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
