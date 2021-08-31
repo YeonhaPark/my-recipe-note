42c6450585253348b334f96c9f15556d3d12ce2d
YeonhaPark <yonaprisca@gmail.com>
2021-09-01 01:08:39 +0900

    wip: comment out signup cypress

--- frontend/cypress/integration/signupPage.js
+++ frontend/cypress/integration/signupPage.js
@@ -7,22 +7,22 @@ describe('renders the signup page', () => {
     cy.get('#signupForm').should('exist');
   });
 
-  it('existing user returns 409', () => {
-    const username = 'yeoni5@gmail.com';
-    const password = 'yeoniyeoni5';
+  // it('existing user returns 409', () => {
+  //   const username = 'yeoni5@gmail.com';
+  //   const password = 'yeoniyeoni5';
 
-    cy.request({
-      method: 'POST',
-      url: 'http://localhost:8081/auth/signup',
-      body: {
-        username,
-        password,
-      },
-      failOnStatusCode: false,
-    }).then((result) => {
-      expect(result.status).to.eq(409);
-    });
-  });
+  //   cy.request({
+  //     method: 'POST',
+  //     url: 'http://localhost:8081/auth/signup',
+  //     body: {
+  //       username,
+  //       password,
+  //     },
+  //     failOnStatusCode: false,
+  //   }).then((result) => {
+  //     expect(result.status).to.eq(409);
+  //   });
+  // });
 
   // it('new user returns 200', () => {
   //   const username = 'yeoni10@gmail.com';
